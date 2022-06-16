bb635e4b8ff9c56db9914ed85121297e5bbf4158
Luiz Felipe Farias <luiz.farias.cpv@gmail.com>
2022-06-16 14:01:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   user: "root",
   host: "localhost",
-  password: "Euodeiosalad@1",
+  password: "ZXCKMasdfadfaskjndFOAFUSEJDJ=",
   database: "nexpdb",
 });
 
